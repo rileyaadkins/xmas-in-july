fb0a1910f3a7bf61c359fdf65c396afd0ef64c85
alisyn-schilling <aeschilling.87@gmail.com>
2020-07-14 13:52:32 -0400

    changed api paths

--- backend-api/src/main/java/org/wcci/apimastery/entities/Artist.java
+++ backend-api/src/main/java/org/wcci/apimastery/entities/Artist.java
@@ -1,8 +1,6 @@
 package org.wcci.apimastery.entities;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
