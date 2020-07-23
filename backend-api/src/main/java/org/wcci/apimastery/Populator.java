ac69465a2eab3772880b372f08855706a41fe8e0
Riley Adkins <rileyaadkins@gmail.com>
2020-07-23 11:51:33 -0400

    all info in populator

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -90,9 +90,25 @@ public class Populator implements CommandLineRunner {
         Song McMc2_11 = new Song("0", "One Child", xmas2);
         Song McMc2_12 = new Song("0", "All I Want for Christmas Is You - Extra Festive", xmas2);
         Song McMc2_13 = new Song("0", "Auld Lang Syne (The New Year's Anthem)", xmas2);
+        songRepo.save(mcMc2_1);
+        songRepo.save(mcMc2_2);
+        songRepo.save(mcMc2_3);
+        songRepo.save(mcMc2_4);
+        songRepo.save(mcMc2_5);
+        songRepo.save(mcMc2_6);
+        songRepo.save(mcMc2_7);
+        songRepo.save(mcMc2_8);
+        songRepo.save(mcMc2_9);
+        songRepo.save(mcMc2_10);
+        songRepo.save(mcMc2_11);
+        songRepo.save(mcMc2_12);
+        songRepo.save(mcMc2_13);
+
 
         /* Last Christmas */
         Song wLc_1 = new Song("4:23", "Last Christmas", lastXmas);
+        songRepo.save(wLc_1);
+
 
         /* Christmas */
         Song mbC_1 = new Song("3:27", "It's Beginning to Look a Lot Like Christmas", xmas3);
@@ -110,6 +126,22 @@ public class Populator implements CommandLineRunner {
         Song mbC_13 = new Song("4:25", "I’ll Be Home for Christmas", xmas3);
         Song mbC_14 = new Song(" 4:00", "Ave Maria", xmas3);
         Song mbC_15 = new Song("4:24", "Mis Deseos / Feliz Navidad (with Thalia)", xmas3);
+        songRepo.save(mbC_1);
+        songRepo.save(mbC_2);
+        songRepo.save(mbC_3);
+        songRepo.save(mbC_4);
+        songRepo.save(mbC_5);
+        songRepo.save(mbC_6);
+        songRepo.save(mbC_7);
+        songRepo.save(mbC_8);
+        songRepo.save(mbC_9);
+        songRepo.save(mbC_10);
+        songRepo.save(mbC_11);
+        songRepo.save(mbC_12);
+        songRepo.save(mbC_13);
+        songRepo.save(mbC_14);
+        songRepo.save(mbC_15);
+        songRepo.save(mbC_16);
 
 
         /* A Charlie Brown Christmas */
@@ -127,34 +159,103 @@ public class Populator implements CommandLineRunner {
         Song cbC_12 = new Song("5:29", "Greensleeves", charlieBrown);
         Song cbC_13 = new Song("2:29", "Great Pumpkin Waltz", charlieBrown);
         Song cbC_14 = new Song("2:00", "Thanksgiving Theme", charlieBrown);
+        songRepo.save(cbC_1);
+        songRepo.save(cbC_2);
+        songRepo.save(cbC_3);
+        songRepo.save(cbC_4);
+        songRepo.save(cbC_5);
+        songRepo.save(cbC_6);
+        songRepo.save(cbC_7);
+        songRepo.save(cbC_8);
+        songRepo.save(cbC_9);
+        songRepo.save(cbC_10);
+        songRepo.save(cbC_11);
+        songRepo.save(cbC_12);
+        songRepo.save(cbC_13);
+        songRepo.save(cbC_14);
 
 
         /* Kids Bops Christmas */
-        Song kbC_1 = new Song("", "It's The Most Wonderful Time Of The Year", kidsBopXmas);
-        Song kbC_2 = new Song("", "Santa Claus Is Coming To Town", kidsBopXmas);
-        Song kbC_3 = new Song("", "All I Want For Christmas Is You", kidsBopXmas);
-        Song kbC_4 = new Song("", "Santa Tell Me", kidsBopXmas);
-        Song kbC_5 = new Song("", "Let It Snow! Let It Snow! Let It Snow!", kidsBopXmas);
-        Song kbC_6 = new Song("", "Have Yourself A Merry Little Christmas", kidsBopXmas);
-        Song kbC_7 = new Song("", "Mistletoe", kidsBopXmas);
-        Song kbC_8 = new Song("", "Rockin' Around The Christmas", kidsBopXmas);
-        Song kbC_9 = new Song("", "Christmas (Baby Please Come Home)", kidsBopXmas);
-        Song kbC_10 = new Song("", "Last Christmas", kidsBopXmas);
-        Song kbC_11 = new Song("", "Rudolph The Red-Nosed Reindeer", kidsBopXmas);
-        Song kbC_12 = new Song("", "Blue Christmas", kidsBopXmas);
-        Song kbC_13 = new Song("", "12 Days Of Christmas", kidsBopXmas);
-        Song kbC_14 = new Song("", "Feliz Navidad", kidsBopXmas);
-        Song kbC_15 = new Song("", "Have A Holly Jolly Christmas", kidsBopXmas);
-        Song kbC_16 = new Song("", "You're A Mean One, Mr. Grinch", kidsBopXmas);
-        Song kbC_17 = new Song("", "Little Saint Nick", kidsBopXmas);
-        Song kbC_18 = new Song("", "Do You Wanna Build A Snowman?", kidsBopXmas);
-        Song kbC_19 = new Song("", "Jingle Bell Rock", kidsBopXmas);
-        Song kbC_20 = new Song("", "Deck The Halls", kidsBopXmas);
-        Song kbC_21 = new Song("", "Silver Bells", kidsBopXmas);
-        Song kbC_22 = new Song("", "Frosty The Snowman", kidsBopXmas);
-        Song kbC_23 = new Song("", "White Christmas", kidsBopXmas);
-        Song kbC_24 = new Song("", "I Wish It Could Be Christmas Everyday", kidsBopXmas);
-        Song kbC_25 = new Song("", "GO CHRISTMAS! (KIDZ BOP Original)", kidsBopXmas);
+        Song kbC_1 = new Song("2:33", "It's The Most Wonderful Time Of The Year", kidsBopXmas);
+        Song kbC_2 = new Song("2:27", "Santa Claus Is Coming To Town", kidsBopXmas);
+        Song kbC_3 = new Song("3:17", "All I Want For Christmas Is You", kidsBopXmas);
+        Song kbC_4 = new Song("3:01", "Santa Tell Me", kidsBopXmas);
+        Song kbC_5 = new Song("2:42", "Let It Snow! Let It Snow! Let It Snow!", kidsBopXmas);
+        Song kbC_6 = new Song("3:21", "Have Yourself A Merry Little Christmas", kidsBopXmas);
+        Song kbC_7 = new Song("2:54", "Mistletoe", kidsBopXmas);
+        Song kbC_8 = new Song("2:07", "Rockin' Around The Christmas", kidsBopXmas);
+        Song kbC_9 = new Song("2:20", "Christmas (Baby Please Come Home)", kidsBopXmas);
+        Song kbC_10 = new Song("3:36", "Last Christmas", kidsBopXmas);
+        Song kbC_11 = new Song("2:37", "Rudolph The Red-Nosed Reindeer", kidsBopXmas);
+        Song kbC_12 = new Song("2:38", "Blue Christmas", kidsBopXmas);
+        Song kbC_13 = new Song("3:53", "12 Days Of Christmas", kidsBopXmas);
+        Song kbC_14 = new Song("2:43", "Feliz Navidad", kidsBopXmas);
+        Song kbC_15 = new Song("2:21", "Have A Holly Jolly Christmas", kidsBopXmas);
+        Song kbC_16 = new Song("2:50", "You're A Mean One, Mr. Grinch", kidsBopXmas);
+        Song kbC_17 = new Song("2:03", "Little Saint Nick", kidsBopXmas);
+        Song kbC_18 = new Song("2:34", "Do You Wanna Build A Snowman?", kidsBopXmas);
+        Song kbC_19 = new Song("2:12", "Jingle Bell Rock", kidsBopXmas);
+        Song kbC_20 = new Song("2:27", "Deck The Halls", kidsBopXmas);
+        Song kbC_21 = new Song("3:06", "Silver Bells", kidsBopXmas);
+        Song kbC_22 = new Song("2:15", "Frosty The Snowman", kidsBopXmas);
+        Song kbC_23 = new Song("2:16", "White Christmas", kidsBopXmas);
+        Song kbC_24 = new Song("3:08", "I Wish It Could Be Christmas Everyday", kidsBopXmas);
+        Song kbC_25 = new Song("3:06", "GO CHRISTMAS! (KIDZ BOP Original)", kidsBopXmas);
+        songRepo.save(kbC_1);
+        songRepo.save(kbC_2);
+        songRepo.save(kbC_3);
+        songRepo.save(kbC_4);
+        songRepo.save(kbC_5);
+        songRepo.save(kbC_6);
+        songRepo.save(kbC_7);
+        songRepo.save(kbC_8);
+        songRepo.save(kbC_9);
+        songRepo.save(kbC_10);
+        songRepo.save(kbC_11);
+        songRepo.save(kbC_12);
+        songRepo.save(kbC_13);
+        songRepo.save(kbC_14);
+        songRepo.save(kbC_15);
+        songRepo.save(kbC_16);
+        songRepo.save(kbC_17);
+        songRepo.save(kbC_18);
+        songRepo.save(kbC_19);
+        songRepo.save(kbC_20);
+        songRepo.save(kbC_21);
+        songRepo.save(kbC_22);
+        songRepo.save(kbC_23);
+        songRepo.save(kbC_24);
+        songRepo.save(kbC_25);
+
+
+        /* Home For Christmas */
+        Song nSC_1 = new Song("4:28", "Home for Christmas", homeForChristmas);
+        Song nSC_2 = new Song("4:32", "Under My Tree", homeForChristmas);
+        Song nSC_3 = new Song("4:45", "I Never Knew the Meaning of Christmas", homeForChristmas);
+        Song nSC_4 = new Song("4:12", "Merry Christmas, Happy Holidays", homeForChristmas);
+        Song nSC_5 = new Song("3:16", "The Christmas Song", homeForChristmas);
+        Song nSC_6 = new Song("3:52", "I Guess It's Christmas Time", homeForChristmas);
+        Song nSC_7 = new Song("3:43", "All I Want This Christmas", homeForChristmas);
+        Song nSC_8 = new Song("3:28", "First Noel", homeForChristmas);
+        Song nSC_9 = new Song("4:06", "In Love on Christmas", homeForChristmas);
+        Song nSC_10 = new Song("4:29", "It's Christmas", homeForChristmas);
+        Song nSC_11 = new Song("3:33", "O Holy Night", homeForChristmas);
+        Song nSC_12 = new Song("3:54", "Love's in Our Hearts on Christmas Day", homeForChristmas);
+        Song nSC_13 = new Song("3:51", "The Only Gift", homeForChristmas);
+        Song nSC_14 = new Song("3:28", "Kiss Me at Midnight", homeForChristmas);
+        songRepo.save(nSC_1);
+        songRepo.save(nSC_2);
+        songRepo.save(nSC_3);
+        songRepo.save(nSC_4);
+        songRepo.save(nSC_5);
+        songRepo.save(nSC_7);
+        songRepo.save(nSC_8);
+        songRepo.save(nSC_9);
+        songRepo.save(nSC_10);
+        songRepo.save(nSC_11);
+        songRepo.save(nSC_12);
+        songRepo.save(nSC_13);
+        songRepo.save(nSC_14);
 
 
     }
